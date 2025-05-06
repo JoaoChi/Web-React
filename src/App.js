b58d1729636c40aa7333300cd3c06303993ec247
João Gabriel <joao.chiarello@unochapeco.edu.br>
2025-05-06 20:37:31 -0300

    Router configurado para ir até a tela de cadastro.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import logo from './loog.webp';
+import { Link } from 'react-router-dom';
 
 function App() {
   const [email, setEmail] = useState('');
@@ -10,30 +11,38 @@ function App() {
     e.preventDefault();
     console.log('Login:', { email, senha });
   };
+  
 
   return (
     <div className="App">
       <div className="login-card">
         <img src={logo} alt="Logo" className="login-logo" />
-        <h2>Bem-vindo!</h2>
+        <h1 className='title'>LOGIN</h1>
         <form onSubmit={handleSubmit}>
+          <text className='text-email'>E-mail</text>
           <input
             type="email"
-            placeholder="Email"
+            placeholder="@gmail.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
           />
+          <text className='text-senha'>Senha</text>
           <input
             type="password"
-            placeholder="Senha"
+            placeholder="senha"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
             required
           />
+          <p className="footer-text">
+            <a href="/esqueci-senha">Esqueci minha senha</a>
+          </p>
           <button type="submit">Entrar</button>
+          <p className="footer-register">
+            Não possui uma conta? <Link to="/cadastro">Registre-se</Link>
+          </p>
         </form>
-        <p className="footer-text">Esqueci minha senha</p>
       </div>
     </div>
   );
---
+++ src/Cadastro.js
@@ -0,0 +1,13 @@
+// src/Cadastro.js
+import React from 'react';
+
+function Cadastro() {
+  return (
+    <div className="cadastro">
+      <h1>Cadastro de Usuário</h1>
+      {/* Adicione aqui o formulário de cadastro */}
+    </div>
+  );
+}
+
+export default Cadastro;
